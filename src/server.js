b44c7d2b1d216e2d8f0c0d92e0a35dfe797190ed
Manjiz <25199230@qq.com>
2016-05-23 13:20:47 +0800

    把重置样式提取到配置文件

--- src/server.js
+++ src/server.js
@@ -78,39 +78,8 @@ router.get('/api/detail/:uuid', function *() {
         try {
           fs.accessSync( buildPath );
         } catch(e) {
-          let commonstyle = '';
-          if(w.platform==='h5') {
-            // 根据AOTU代码规范重置样式
-            commonstyle = `
-* { -webkit-tap-highlight-color: transparent; outline: 0; margin: 0; padding: 0; vertical-align: baseline; }
-body, h1, h2, h3, h4, h5, h6, hr, p, blockquote, dl, dt, dd, ul, ol, li, pre, form, fieldset, legend, button, input, textarea, th, td { margin: 0; padding: 0; vertical-align: baseline; }
-img { border: 0 none; vertical-align: top; }
-i, em { font-style: normal; }
-ol, ul { list-style: none; }
-input, select, button, h1, h2, h3, h4, h5, h6 { font-size: 100%; font-family: inherit; }
-table { border-collapse: collapse; border-spacing: 0; }
-a { text-decoration: none; color: #666; }
-body { margin: 0 auto; min-width: 320px; max-width: 640px; height: 100%; font-size: 14px; font-family: Helvetica, STHeiti STXihei, Microsoft JhengHei, Microsoft YaHei, Arial; line-height: 1.5; color: #666; -webkit-text-size-adjust: 100% !important; text-size-adjust: 100% !important; }
-input[type="text"], textarea { -webkit-appearance: none; -moz-appearance: none; appearance: none; }`;
-          } else if(w.platform==='pc') {
-            commonstyle = `
-html, body, div, h1, h2, h3, h4, h5, h6, p, dl, dt, dd, ol, ul, li, fieldset, form, label, input, legend, table, caption, tbody, tfoot, thead, tr, th, td, textarea, article, aside, audio, canvas, figure, footer, header, mark, menu, nav, section, time, video { margin: 0; padding: 0; }
-h1, h2, h3, h4, h5, h6 { font-size: 100%; font-weight: normal }
-article, aside, dialog, figure, footer, header, hgroup, nav, section, blockquote { display: block; }
-ul, ol { list-style: none; }
-img { border: 0 none; vertical-align: top; }
-blockquote, q { quotes: none; }
-blockquote:before, blockquote:after, q:before, q:after { content: none; }
-table { border-collapse: collapse; border-spacing: 0; }
-strong, em, i { font-style: normal; font-weight: normal; }
-ins { text-decoration: underline; }
-del { text-decoration: line-through; }
-mark { background: none; }
-input::-ms-clear { display: none !important; }
-body { font: 12px/1.5 \\5FAE\\8F6F\\96C5\9ED1, \\5B8B\\4F53, "Hiragino Sans GB", STHeiti, "WenQuanYi Micro Hei", "Droid Sans Fallback", SimSun, sans-serif; background: #fff; }
-a { text-decoration: none; color: #333; }
-a:hover { text-decoration: underline; }`;
-          }
+          // 根据AOTU代码规范重置样式
+          let commonstyle = conf.tpl[`css${w.platform}`] || '';
           let iframe = `
 <!DOCTYPE html>
 <html lang="en">
@@ -139,7 +108,6 @@ a:hover { text-decoration: underline; }`;
           fs.writeFileSync( path.join(buildPath, 'index.html'), iframe);
           // 图片
           fs.mkdirSync( bimg );
-          console.log(wpimg, bimg)
           fstream
             .Reader( wpimg )
             .pipe( fstream.Writer( bimg ) );
