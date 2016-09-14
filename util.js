86906b227d123be9f736f8ab268725318b71bd76
Manjiz <25199230@qq.com>
2016-09-14 13:03:40 +0800

    update 组件编译部分代码更扁平，增加回滚处理

--- util.js
+++ util.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
+const fse = require('fs-extra');
 const unzip = require('unzip');
 const fstream = require('fstream');
 const lodash = require('lodash');
@@ -16,6 +17,16 @@ function getDate () {
     ('0' + ts.getHours()).slice(-2) + ':' +
     ('0' + ts.getMinutes()).slice(-2) + ':' +
     ('0' + ts.getSeconds()).slice(-2);
+}
+
+// 文件是否存在
+exports.existsSync = function (pd) {
+	try {
+		fs.accessSync(pd);
+		return true;
+	} catch(err) {
+		return false;
+	}
 }
 
 /**
@@ -66,96 +77,129 @@ exports.buildWidget = function (id, widget) {
 		if(!id) {reject('没有提供组件ID'); return;}
 
 		// 编译路径
-		let widgetBuiltPath = path.join(conf.warehouse, '_build', id);
+		const widgetBuiltPath = path.join(conf.warehouse, '_build', id);
 		// 编译图片路径
-		let widgetBuiltImgPath = path.join(widgetBuiltPath, 'images');
+		const widgetBuiltImgPath = path.join(widgetBuiltPath, 'images');
 		// 编译整合信息文件（为减少文件读写次数而生）
-		let widgetBuiltIntegratePath = path.join(widgetBuiltPath, 'integrate.json');
+		const widgetBuiltIntegratePath = path.join(widgetBuiltPath, 'integrate.json');
 		// 编译预览HTML路径
-		let widgetBuiltHtmlPath = path.join(widgetBuiltPath, 'index.html');
+		const widgetBuiltHtmlPath = path.join(widgetBuiltPath, 'index.html');
 
-		try {
-		  fs.accessSync( widgetBuiltPath );
-		  // fs.accessSync( widgetBuiltIntegratePath );
-		} catch(err) {
-			try {
-				fs.mkdirSync(widgetBuiltPath);
-				fs.mkdirSync(widgetBuiltImgPath);
-			} catch(e) {
-				reject(e);
-				return;
-			}
-
-			// 所有内容整合到一个文件
-			let integrate = {};
-			let contHtml, contBuiltHtml, contScss, contBuiltCss, contCss, contJs, contJson;
-
-			// 解压路径
-			let widgetTempPath = path.join(conf.warehouse, '_temp', id);
-			// 解压图片路径
-			let widgetTempImgPath = path.join(widgetTempPath, 'images');
-			// 解压HTML路径
-			let contHtmlPath = path.join(widgetTempPath, widget.get('name') + '.html');
-			// 解压SCSS路径
-			let contScssPath = path.join(widgetTempPath, widget.get('name') + '.scss');
-			// 解压CSS路径 - 优先用编译好的CSS文件
-			let contBuiltCssPath = path.join(widgetTempPath,  '_build_' + widget.get('name') + '.css');
-			let contCssPath = path.join(widgetTempPath, widget.get('name') + '.css');
-			// 解压JS路径
-			let contJsPath = path.join(widgetTempPath, widget.get('name') + '.js');
-			// 解压JSON路径
-			let contJsonPath = path.join(widgetTempPath, widget.get('name') + '.json');
-
-			// 读取组件 HTML, SCSS, CSS, JS
-		  try {contHtml = fs.readFileSync(contHtmlPath).toString(); integrate.contHtml = contHtml;} catch(err) { /* DO NOTHING */ }
-		  try {contScss = fs.readFileSync(contScssPath).toString(); integrate.contScss = contScss;} catch(err) { /* DO NOTHING */ }
-		  try {contBuiltCss = fs.readFileSync(contBuiltCssPath).toString(); integrate.contBuiltCss = contBuiltCss;} catch(err) { /* DO NOTHING */ }
-		  try {contCss = fs.readFileSync(contCssPath).toString(); integrate.contCss = contCss;} catch(err) { /* DO NOTHING */ }
-		  try {contJs = fs.readFileSync(contJsPath).toString(); integrate.contJs = contJs;} catch(err) { /* DO NOTHING */ }
-		  try {contJson = fs.readFileSync(contJsonPath).toString(); integrate.contJson = contJson;} catch(err) { /* DO NOTHING */ }
-
-		  if(contHtml) {
-		  	contHtml = contHtml.replace('<% widget.scriptStart() %>', '').replace('<% widget.scriptEnd() %>', '');
-				try {
-	        // 根据配置里提供的变量进行编译
-	        integrate.contBuiltHtml = contBuiltHtml = lodash.template(contHtml)(
-	          JSON.parse(contJson).data
-	        );
-	      } catch(err) {
-	        console.error('模板渲染错误：' + err);
-	      }
-
-	      // 写1/4：整合信息的文件 [异步完成]
-		  	fs.writeFile(widgetBuiltIntegratePath, JSON.stringify(integrate), function(err) {
-		  		console.error(err);
-		  	});
-
-	      let commonstyle = conf.tpl[`css${widget.get('platform')}`] || '';
-				let iframe = `<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>Document</title><style>${commonstyle};${contBuiltCss || contCss || ''}</style></head><body>${contBuiltHtml}<script>${contJs}</script></body></html>`;
-
-	      // 写2/4：预览HTML
-	      fs.writeFileSync(widgetBuiltHtmlPath, iframe);
-
-	      // 写3/4：图片
-	      let writer = fstream.Writer(widgetBuiltImgPath);
-        fstream
-          .Reader(widgetTempImgPath)
-          .pipe(writer);
-        writer.on('close', function () {
-          resolve(integrate);
-        });
-
-        // 写4/4：网页截图 [异步完成]
-				let instance = await phantom.create(['--ignore-ssl-errors=true', '--local-to-remote-url-access=true']);
-		  	let page = await instance.createPage();
-		  	await page.property('viewportSize', {width: 375, height: 667});
-		  	await page.open('file:///' + path.resolve(`${conf.warehouse}/_build/${id}/index.html`));
-		  	await page.render(`${conf.warehouse}/_build/${id}/capture.png`);
-		  	instance.exit();
-		  }
+		// 如果有，直接返回
+		if(this.existsSync(widgetBuiltIntegratePath)) {
+			resolve(JSON.parse(fs.readFileSync(widgetBuiltIntegratePath).toString()));
+			return;
 		}
 
-		resolve(JSON.parse(fs.readFileSync(widgetBuiltIntegratePath).toString()));
+		// 定义 回滚处理
+		const rollback = function () {
+			fse.remove(widgetBuiltPath, function(err) {
+				if(err) {console.error(err);}
+			});
+		}
+		// 定义 写整合文件
+		const writeIntegrate = function (integrate) {
+			try {
+				fs.writeFileSync(widgetBuiltIntegratePath, JSON.stringify(integrate));
+				return true;
+			} catch(err) {
+				console.error(err);
+				return false;
+			}
+		}
+
+		// 创建编译目录
+		try {
+			fse.emptyDirSync(widgetBuiltPath);
+			fse.emptyDirSync(widgetBuiltImgPath);
+		} catch(err) {
+			reject(err);
+			console.error(err);
+			rollback(err);
+			return;
+		}
+
+		// 所有内容整合到一个文件
+		let integrate = {};
+		let contHtml, contBuiltHtml, contScss, contBuiltCss, contCss, contJs, contJson;
+		// 解压路径
+		let widgetTempPath = path.join(conf.warehouse, '_temp', id);
+		// 解压图片路径
+		let widgetTempImgPath = path.join(widgetTempPath, 'images');
+		// 解压HTML路径
+		let contHtmlPath = path.join(widgetTempPath, widget.get('name') + '.html');
+		// 解压SCSS路径
+		let contScssPath = path.join(widgetTempPath, widget.get('name') + '.scss');
+		// 解压CSS路径 - 优先用编译好的CSS文件
+		let contBuiltCssPath = path.join(widgetTempPath,  '_build_' + widget.get('name') + '.css');
+		let contCssPath = path.join(widgetTempPath, widget.get('name') + '.css');
+		// 解压JS路径
+		let contJsPath = path.join(widgetTempPath, widget.get('name') + '.js');
+		// 解压JSON路径
+		let contJsonPath = path.join(widgetTempPath, widget.get('name') + '.json');
+
+		// 读取组件 HTML, SCSS, CSS, JS
+		try {contHtml = fs.readFileSync(contHtmlPath).toString(); integrate.contHtml = contHtml;} catch(err) { /* DO NOTHING */ }
+		try {contScss = fs.readFileSync(contScssPath).toString(); integrate.contScss = contScss;} catch(err) { /* DO NOTHING */ }
+		try {contBuiltCss = fs.readFileSync(contBuiltCssPath).toString(); integrate.contBuiltCss = contBuiltCss;} catch(err) { /* DO NOTHING */ }
+		try {contCss = fs.readFileSync(contCssPath).toString(); integrate.contCss = contCss;} catch(err) { /* DO NOTHING */ }
+		try {contJs = fs.readFileSync(contJsPath).toString(); integrate.contJs = contJs;} catch(err) { /* DO NOTHING */ }
+		try {contJson = fs.readFileSync(contJsonPath).toString(); integrate.contJson = contJson;} catch(err) { /* DO NOTHING */ }
+
+		// 如果没有HTML就没必要编译
+		if(!contHtml) {
+			if(!writeIntegrate(integrate)) {
+				rollback();
+				// 此处虽然写失败，不需要return中断，因为内容仍存在变量中
+			}
+			resolve(integrate);
+			return;
+		}
+
+		// HTML编译，变量在配置文件的data字段提供
+		contHtml = contHtml.replace('<% widget.scriptStart() %>', '').replace('<% widget.scriptEnd() %>', '');
+		try {
+		  integrate.contBuiltHtml = contBuiltHtml = lodash.template(contHtml)(
+		    JSON.parse(contJson).data
+		  );
+		} catch(err) {
+		  console.error('模板渲染错误：' + err);
+		}
+
+		// 提前返回
+		resolve(integrate);
+
+		// 写整合文件
+		if(!writeIntegrate(integrate)) {
+			rollback();
+			return;
+		}
+		
+		// 写预览HTML文件
+		let commonstyle = conf.tpl[`css${widget.get('platform')}`] || '';
+		let iframe = `<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>Document</title><style>${commonstyle};${contBuiltCss || contCss || ''}</style></head><body>${contBuiltHtml}<script>${contJs}</script></body></html>`;
+		try {
+			fs.writeFileSync(widgetBuiltHtmlPath, iframe);
+		} catch(err) {
+			console.error(err);
+			rollback(err);
+			return;
+		}
+
+		// 复制图片
+		let reader = fstream.Reader(widgetTempImgPath);
+		let writer = fstream.Writer(widgetBuiltImgPath);
+		reader.pipe(writer);
+		reader.on('error', function () { rollback(); })
+		writer.on('close', function () { });
+		
+		// 网页截图
+		let instance = await phantom.create(['--ignore-ssl-errors=true', '--local-to-remote-url-access=true']);
+		let page = await instance.createPage();
+		// await page.property('viewportSize', {width: 375, height: 667});
+		await page.open('file:///' + path.resolve(`${conf.warehouse}/_build/${id}/index.html`));
+		await page.render(`${conf.warehouse}/_build/${id}/capture.png`);
+		instance.exit();
 	});
 }
 
