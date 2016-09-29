501efe0c2f58c611791ab0f2e89d725654a20f67
Manjiz <25199230@qq.com>
2016-09-29 10:58:56 +0800

    预览html加移动端META

--- util.js
+++ util.js
@@ -176,7 +176,22 @@ exports.buildWidget = function (id, widget) {
 		
 		// 写预览HTML文件
 		let commonstyle = conf.tpl[`css${widget.get('platform')}`] || '';
-		let iframe = `<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>Document</title><style>${commonstyle}${contBuiltCss || contCss || ''}</style></head><body>${contBuiltHtml}<script>${contJs}</script></body></html>`;
+		let iframe = `<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+<meta name="format-detection" content="telephone=no" />
+<title>Document</title>
+<style>${commonstyle}${contBuiltCss || contCss || ''}</style>
+</head>
+<body>
+${contBuiltHtml}
+<script>${contJs}</script>
+</body>
+</html>`;
 		try {
 			fs.writeFileSync(widgetBuiltHtmlPath, iframe);
 		} catch(err) {
