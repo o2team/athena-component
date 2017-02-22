94d341a189977d6e51c3b0de6469699bc9e3fe3b
Manjiz <25199230@qq.com>
2017-02-22 14:46:06 +0800

    chore: 拆分bundle.js为vendor.js和app.js
    原bundle.js过大，而服务器经常歇菜，所以弄拆分一下

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,14 +1,28 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   context: __dirname,
   entry: {
-    app: './src/app.js'
+    app: './src/app.js',
+    vendor: [
+      'vue',
+      'vue-router',
+      'vuex',
+      'vue-resource',
+      'vue-lazyload',
+      'syntaxhighlighter',
+      'brush-css',
+      'brush-sass',
+      'brush-javascript',
+      'brush-xml'
+    ]
   },
   output: {
+    publicPath: '/dist/',
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist/'
+    filename: '[name].js',
+    chunkFilename: '[name].[hash].js'
   },
   module: {
     loaders: [
@@ -22,6 +36,14 @@ module.exports = {
         exclude: /node_modules/
       },
       {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [
+          path.resolve(__dirname, 'node_modules/syntaxhighlighter'),
+          path.resolve(__dirname, 'node_modules/brush-base')
+        ]
+      },
+      {
         test: /\.scss$/,
         loader: 'style!css!sass'
       },
@@ -30,5 +52,11 @@ module.exports = {
         loader: 'url-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor'],
+      minChunks: Infinity
+    })
+  ],
 }
